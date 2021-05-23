223f83be0087e82821144c300415b4c24eee80d2
Terry Theis <tltheis@prodigy.net>
2021-05-23 07:55:41 -0500

    new post

---
+++ public/javascript/create-post.js
@@ -0,0 +1,26 @@
+async function postHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const post_text = document.querySelector('textarea[name="post-body"]').value.trim();
+
+    const response = await fetch('/api/post', {
+        method: 'post',
+        body: JSON.stringify({
+            title,
+            post_text
+        }),
+
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('#create-post').addEventListener('submit', postHandler);
