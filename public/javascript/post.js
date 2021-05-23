410d617bd38da2d7d103ab39737f1477b4e9b6c8
Terry Theis <tltheis@prodigy.net>
2021-05-22 20:49:47 -0500

    dashboard

--- public/javascript/post.js
+++ public/javascript/post.js
@@ -1,10 +1,10 @@
 async function postHandler(event) {
     event.preventDefault();
   
-    const title = document.querySelector('input[name="post-title"]').value;
-    const post_text = document.querySelector('input[name="post-text"]').value;
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const post_text = document.querySelector('textarea[name="post-body"]').value.trim();
   
-    const response = await fetch(`/api/post`, {
+    const response = await fetch('/api/post/', {
       method: 'post',
       body: JSON.stringify({
         title,
